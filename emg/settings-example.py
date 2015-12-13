dbf2562bc4ea3681754ce60b82ed8b254ea1d2c8
jatchili <jatchili@users.noreply.github.com>
2015-12-13 05:03:45 -0600

    Update settings-example.py

--- emg/settings-example.py
+++ emg/settings-example.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'some-random-string'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','yourdomain.example.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
